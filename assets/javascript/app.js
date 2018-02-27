ac69dedcb7ae5489b3b2993c16efcd6b2a2cdf7f
Cody Haines <chaines51@gmail.com>
2018-02-27 17:35:28 -0600

    Finished

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,12 +2,14 @@
 // Declaring global variables
 // Constant variables
 var TOTAL_QUESTIONS = 5;
-var TIME_LEFT = 30;
+var TIME_LEFT = 2;
 // Variables that will change
 var correct = 0;
 var incorrect = 0;
 var unanswered = 0;
 var begin = false;
+var timeLeft = TIME_LEFT;
+var timer;
 
 // Functions
 
@@ -15,38 +17,71 @@ var begin = false;
 
 // This function will hide the start screen and show the trivia game and start the timer.
 // To do: add timer start
-function renderGame() {
-    $('#startScreen').css('display','none');
-    $('.resultScreen').css('display','none');
-    $('.quizElement').css('display','inline-block');
-};
-
-function results() {
-    $('.quizElement').css('display','none');
-    $('.resultScreen').css('display','inline-block');
-    $('#correct').text('Correct: ' + correct);
-    $('#incorrect').text('Incorrect: ' + incorrect);
-    $('#unanswered').text('Unanswered: ' + unanswered);
+function startGame() {
+  correct = 0;
+  incorrect = 0;
+  unanswered = 0;
+  begin = false;
+  timeLeft = TIME_LEFT;
+  timer = setInterval(tick, 1000);
+  $("#timer").text(timeLeft);
+  $("#startScreen").css("display", "none");
+  $(".resultScreen").css("display", "none");
+  $(".quizElement").css("display", "inline-block");
 }
 
+function tick() {
+  timeLeft--;
+  if (timeLeft == 0) {
+    clearInterval(timer);
+    results();
+  } else {
+    $("#timer").text(timeLeft);
+  }
+}
 
-
-
-
+function results() {
+  $(".quizElement").css("display", "none");
+  $(".resultScreen").css("display", "inline-block");
+  $(".quizQuestion").each(function() {
+    var questionCorrect = false;
+    var questionUnanswered = true;
+    $(this)
+      .children("input[type=radio]")
+      .each(function() {
+        if (this.checked) {
+          if (this.value == "right") {
+            questionCorrect = true;
+          }
+          questionUnanswered = false;
+        }
+      });
+    if (questionUnanswered) {
+      unanswered++;
+    } else if (questionCorrect) {
+      correct++;
+    } else {
+      incorrect++;
+    }
+  });
+  $("#correct").text("Correct: " + correct);
+  $("#incorrect").text("Incorrect: " + incorrect);
+  $("#unanswered").text("Unanswered: " + unanswered);
+}
 
 // Logic of the game
 // This block of code is what detects the click and runs the renderGame function to start the game.
-$('#begin').on("click", function(startGame){
-    renderGame();
+$("#begin").on("click", function(e) {
+  startGame();
 });
 
 // To do: Make an if statement to detect Correct/Incorrect/Unanswered results when the "Submit" button is clicked.
 
 // To do: Display the results on a different screen after submitting the quiz.
-$('#submitButton').on('click', function(showResults){
-    results();
+$("#submitButton").on("click", function(showResults) {
+  results();
 });
 // "Start Over" button restarts the game.
-$('#restart').on('click', function(restartGame){
-    renderGame();
+$("#restart").on("click", function(restartGame) {
+  startGame();
 });
